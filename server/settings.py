f5922f9d8e34b9ff19b2a8aa15ca2dd5295c76f4
caiogn-dev <caiogn2002@gmail.com>
2026-01-03 19:10:06 -0300

    bucket......

--- server/settings.py
+++ server/settings.py
@@ -240,7 +240,7 @@ LOGGING = {
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY') 
 AWS_STORAGE_BUCKET_NAME = 'pastita-final' # O NOME NOVO AQUI
-AWS_S3_REGION_NAME = 'us-east-1' 
+AWS_S3_REGION_NAME = 'sa-east-1' 
 
 # Forçar Storage
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
