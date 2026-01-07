c2250c697f4694da069cf11a73cd62d2500b726b
caiogn-dev <caiogn2002@gmail.com>
2026-01-07 20:03:39 -0300

    ok

--- server/asgi.py
+++ server/asgi.py
@@ -9,14 +9,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from api.routing import websocket_urlpatterns
 from api.ws_auth import TokenAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 django_asgi_app = get_asgi_application()
 
